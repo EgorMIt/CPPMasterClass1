4a155671ec7ce2fb72350fed09d9110409c67212
EgorMIt <egor.mitrofanov@hotmail.com>
2021-03-20 00:31:17 +0300

    Все заработало

--- CPPMasterClass1/Main.c
+++ CPPMasterClass1/Main.c
@@ -1,118 +1,71 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
-#define PE_SIG_OFFSET_LOCATION 0x3c
-
 #include <stdio.h>
-#include <Windows.h>
-#include <stdlib.h>
-
+#include <windows.h>
 
 int main() {
+    FILE* fileInputStream = fopen("Ex1.exe", "rb");
+    FILE* fileOutputStreamTxt = fopen("D:\\outputinfo.txt", "w");  
+    FILE* fileOutputStreamCode = fopen("D:\\code.txt", "wb");
 
-    DWORD FileSize, PeHeaderAddress, Signature, exports, Characteristics_export, sectionNumber;
-    errno_t err;
-    FILE* fp = NULL;
-    err = fopen_s(&fp, "D:\\Ex1.exe", "rb");
-
-    IMAGE_DOS_HEADER DosHeader = { 0 };
-    PIMAGE_DOS_HEADER dosHeader = { 0 };
-    PIMAGE_NT_HEADERS PeHeader = { 0 };
-    IMAGE_FILE_HEADER FileHeader = { 0 };
-    IMAGE_OPTIONAL_HEADER OpHeader = { 0 };
-    IMAGE_SECTION_HEADER SectionHeader = { 0 };
-    IMAGE_EXPORT_DIRECTORY Export = { 0 };
-
-
-
-    fseek(fp, 0, SEEK_END);
-    FileSize = ftell(fp);
-
-    if (FileSize < sizeof(IMAGE_DOS_HEADER) + sizeof(IMAGE_NT_HEADERS))
-        printf("NOT PE file ");
-    else
-        printf("Reading PE FILE\n----------------------\n\n");
-
-    printf("Reading DOS header \n----------------------\n");
-    printf("size of DosHeder:\t\t %d \n", sizeof DosHeader); //sizeofDosHeader
-
-    fseek(fp, 0, SEEK_SET);
-    fread(&DosHeader, sizeof DosHeader, 1, fp);
-    if (DosHeader.e_magic != 0x5a4d)
-        printf("NOT PE file");
-
-    printf("Magic number:\t\t\t MZ(%#x) \n", DosHeader.e_magic);    // e_magic
-    printf("Adress of PE header(e_lfanew):\t %#xh \n", DosHeader.e_lfanew);     // e_lfanew
-    printf("----------------------\n\n");
-    PeHeaderAddress = DosHeader.e_lfanew;
-
-
-    if (FileSize <= PeHeaderAddress + sizeof(IMAGE_NT_HEADERS))
-        printf("NOT PE file");
-    else
-        printf("Reading PE Header \n----------------------\n");
-
-    
-    printf("\nPE File signature \n----------------------\n");
-    fseek(fp, PeHeaderAddress, SEEK_SET);          
-    fread(&Signature, sizeof(DWORD), 1, fp);      
-    printf("Signature:\t\t %#x \n", Signature);     //Signature
-
-
-    fread(&FileHeader, sizeof FileHeader, 1, fp);       //FileHeader
-    sectionNumber = FileHeader.NumberOfSections;//NumberOfSection
-
-    printf("number of section:\t %d \n", sectionNumber);
-    printf("Size of optional Header: %d \n", FileHeader.SizeOfOptionalHeader);  //SizeOfOptionalHeader
-    printf("Characteristics:\t\t %x \n", FileHeader.Characteristics);
-
-    
-    printf("\nPE Optional Header \n----------------------\n");
-
-    fread(&OpHeader, 1, sizeof OpHeader, fp);            
-
-    printf("Address of Entry Point:\t\t %#x\n", OpHeader.AddressOfEntryPoint);
-    
-    printf("----------------------\n\n");
-    
-    //fread(&SectionHeader, PeHeaderAddress + sizeof(IMAGE_NT_HEADERS), sizeof SectionHeader, fp);    //sectionHeader
-
-    int secCount = 1;     
-    printf(sectionNumber);
-    /*
-    
-    while (secCount <= FileHeader.NumberOfSections)     
-    {
-        printf("section header (%d or %d) \n", secCount, FileHeader.NumberOfSections);
-        printf("---------------------\n");
-        printf("Section Header name \t\t:%s\n", SectionHeader.Name);
-        printf("Virtual Size  \t\t\t:%#x\n", SectionHeader.Misc.VirtualSize);
-        printf("Virtual Address \t\t:%#x\n", SectionHeader.VirtualAddress);
-        printf("Size of raw data  \t\t:%#x\n", SectionHeader.SizeOfRawData);
-        printf("Pointer to Raw Data \t\t:%#x\n\n", SectionHeader.PointerToRawData);
-        fseek(fp, PeHeaderAddress + sizeof(IMAGE_NT_HEADERS) + secCount * sizeof(IMAGE_SECTION_HEADER), SEEK_SET);
-        fread(&SectionHeader, sizeof SectionHeader, 1, fp);
-        secCount++;
+    if (!fileInputStream) {
+        printf("File not found");
+        return 1;
     }
 
-    printf("Reading ExPort \n----------------------\n");
-    exports = SectionHeader.Characteristics;
-    fseek(fp, exports, SEEK_SET);                           //IMAGE_EXPORT 
-    fread(&Characteristics_export, sizeof(DWORD), 1, fp);
-    printf("characteristics \t: %#x \n", Characteristics_export);
+    IMAGE_DOS_HEADER DosHeader = { 0 };
+    IMAGE_NT_HEADERS PeHeader = { 0 };
+    IMAGE_SECTION_HEADER SectionHeader = { 0 };
+    IMAGE_SECTION_HEADER TxtSectionHeader = { 0 };
 
-    fread(&Export, sizeof Export, 1, fp);                   //IMAGE_EXPORT
-    printf("nName \t\t\t: %x \n", Export.Name);
-    printf("nBase \t\t\t: %x \n", Export.Base);
-    printf("Number of Function \t: %d \n", Export.NumberOfFunctions);
-    printf("Number of Name \t\t: %d \n", Export.NumberOfNames);
-    printf("Address of function \t: %x \n", Export.AddressOfFunctions);
-    printf("Adress Of Name \t\t: %x \n", Export.AddressOfNames);
-    printf("Address of name ordinals: %x \n", Export.AddressOfNameOrdinals);
+    fread(&DosHeader, sizeof DosHeader, 1, fileInputStream);
 
-    
-
-    */
+    if (DosHeader.e_magic != 0x5a4d) {
+        printf("file should be PE");
+    }
 
 
-    fclose(fp);
-    system("pause");
+    fseek(fileInputStream, DosHeader.e_lfanew, 0);
+    fread(&PeHeader, sizeof(IMAGE_NT_HEADERS), 1, fileInputStream);
+    fprintf(fileOutputStreamTxt, "Signature:\t\t %lx \n", PeHeader.Signature);
+
+    fprintf(fileOutputStreamTxt, "Numbers of sections:\t %d\n", PeHeader.FileHeader.NumberOfSections);
+   
+
+    fprintf(fileOutputStreamTxt, "Entry point address:\t %08lu\n\n", PeHeader.OptionalHeader.AddressOfEntryPoint);
+
+    for (int i = 0; i < PeHeader.FileHeader.NumberOfSections; ++i) {
+
+        fread(&SectionHeader, sizeof(IMAGE_SECTION_HEADER), 1, fileInputStream);
+        fprintf(fileOutputStreamTxt, "-------------------------------\n");
+        fprintf(fileOutputStreamTxt, "%2d %s VirtSize:\t  %08lx\n\n", i + 1, SectionHeader.Name, SectionHeader.Misc.VirtualSize);
+        fprintf(fileOutputStreamTxt, "%s %08lx\n", "VirtualAddress:\t\t", SectionHeader.VirtualAddress);
+        fprintf(fileOutputStreamTxt, "%2s %08lx\n", "Raw data offset:\t", SectionHeader.PointerToRawData);
+        fprintf(fileOutputStreamTxt, "%2s %08lx\n", "Size of raw data:\t", SectionHeader.SizeOfRawData);
+        fprintf(fileOutputStreamTxt, "%2s %08lx\n", "Relocation offset:\t", SectionHeader.PointerToRelocations);
+        fprintf(fileOutputStreamTxt, "%2s %08x\n", "Relocations number:\t", SectionHeader.NumberOfRelocations);
+        fprintf(fileOutputStreamTxt, "%2s %08lx\n", "Line offset:\t\t", SectionHeader.PointerToLinenumbers);
+        fprintf(fileOutputStreamTxt, "%2s %08x\n", "Lines number:\t\t", SectionHeader.NumberOfLinenumbers);
+        fprintf(fileOutputStreamTxt, "%2s %08lx\n\n", "Characteristics:\t", SectionHeader.Characteristics);
+
+        if (strcmp((const char*)SectionHeader.Name, ".text") == 0) {
+            TxtSectionHeader = SectionHeader;
+        }
+
+    }
+
+    fseek(fileInputStream, TxtSectionHeader.PointerToRawData, 0);
+
+    char* data = malloc(TxtSectionHeader.SizeOfRawData);
+
+    fread(data, sizeof(char), TxtSectionHeader.SizeOfRawData, fileInputStream);
+    fwrite(data, sizeof(char), TxtSectionHeader.SizeOfRawData, fileOutputStreamCode);
+    free(data);
+
+
+    fclose(fileOutputStreamTxt);
+    fclose(fileInputStream);
+    fclose(fileOutputStreamCode);
+
+    printf("Section data is written in outputinfo.txt\nBinary code is written to code.txt\nShutting down...\n\n");
+    return 0;
 }
